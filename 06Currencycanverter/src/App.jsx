b9eb3039f0bd2af8666b02b3d36c7e199c91da2d
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-03-10 22:20:52 +0530

    do

--- 06Currencycanverter/src/App.jsx
+++ 06Currencycanverter/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+        backgroundImage: `url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}
     >
       <div className="w-full">
