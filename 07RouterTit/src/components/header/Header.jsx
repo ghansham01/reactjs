045a4ed87aee95c8b9cf395b787dbe03a15ff6ea
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-03-11 16:53:24 +0530

    router3

--- 07RouterTit/src/components/header/Header.jsx
+++ 07RouterTit/src/components/header/Header.jsx
@@ -54,12 +54,12 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                    to={'contect'}
+                                    to={'contact'}
                                     className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 ${isActive} duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
-                                    Contect
+                                    Contact
                                 </NavLink>
                             </li>
                             <li>
