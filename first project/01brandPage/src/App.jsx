fe40e7eef08c6de4aeb74cd9525e7407e1852689
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-03-26 18:39:48 +0530

    theeee

---
+++ first project/01brandPage/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import './assets/styles/index.css';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
