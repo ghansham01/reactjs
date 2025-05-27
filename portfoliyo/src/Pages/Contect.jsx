9385bdca5cb3cf5b76ed3cabfb7a2e441003494b
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-05-27 11:23:49 +0530

    deleted

--- portfoliyo/src/Pages/Contect.jsx
+++
@@ -1,9 +0,0 @@
-import MotionHoc from "./MotionHoc";
-
-const ContectComponent = () => {
-  return <h1>Contects</h1>;
-};
-
-const Contects = MotionHoc(ContectComponent);
-
-export default Contects;
